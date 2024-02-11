97e7074f147395ebbda68e630fd0b3fb8edc9ef3
Abdullah-2238 <155958204+Abdullah-2238@users.noreply.github.com>
2024-02-12 01:16:14 +0500

    Delete Assignment 6 directory

--- Assignment 6/app.js
+++
@@ -1,3 +0,0 @@
-btn.addEventListener("click",() => alert("button clicked!"))
-divv.addEventListener("mouseover",() => divv.style = "background-color:black");
-divv.addEventListener("mouseout",() => divv.style = "");
